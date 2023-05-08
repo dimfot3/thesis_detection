75c5e2d7e1f800a5ceb338f59357d43b3ecdc6ab
dimitrios fotiou <fotiou.dimitris3@gmail.com>
2023-05-09 01:19:34 +0300

    human detector for eval

--- human_detector.py
+++ human_detector.py
@@ -19,10 +19,12 @@ from geometry_msgs.msg import PointStamped
 import torch
 from scipy.spatial import KDTree
 from sklearn.cluster import DBSCAN
+from builtin_interfaces.msg import Time
+from collections import Counter
 
 
 class HumanDetector(Node):
-    def __init__(self, lidar_list, det_model):
+    def __init__(self, lidar_list, det_model, max_hum=None):
         super().__init__('human_detector')
         self.lidar_list = lidar_list
         self.lidar_frames, self.lidar_pcl, self.lidar_times = {}, {}, {}
@@ -37,7 +39,8 @@ class HumanDetector(Node):
         self.pub_cls = self.create_publisher(PointCloud2, '/lidar_clustering', 10)
         self.human_seg_pub = self.create_publisher(PointCloud2, '/human_seg', 10)
         self.human_dot_pub = self.create_publisher(PointCloud2, '/human_detections', 10)
-        self.det_thresh = 0.8
+        self.det_thresh = 0.5
+        self.max_hum=max_hum
         
     def read_lidar_frames(self, msg):
         tf_mat = self.tfmsg_to_matrix(msg)
@@ -106,11 +109,14 @@ class HumanDetector(Node):
         msg.data = pcl.tobytes()
         self.pub_cls.publish(msg)        
 
-    def publish_humans(self, human_poses):
+    def publish_humans(self, human_poses, pcl_time):
         if(human_poses.shape[0] < 1):
             return
         msg = PointCloud2()
-        msg.header.stamp = self.get_clock().now().to_msg()
+        time_msg = Time()
+        time_msg.sec = int(pcl_time)
+        time_msg.nanosec = int((pcl_time - time_msg.sec) * 1e9)
+        msg.header.stamp = time_msg
         msg.header.frame_id = "world"
         msg.fields.append(PointField(name="x", offset=0, datatype=PointField.FLOAT32, count=1))
         msg.fields.append(PointField(name="y", offset=4, datatype=PointField.FLOAT32, count=1))
@@ -124,12 +130,15 @@ class HumanDetector(Node):
         msg.data = human_poses.astype(np.float32).tobytes()
         self.human_dot_pub.publish(msg)        
 
-    def publish_human_seg(self, human_points):
+    def publish_human_seg(self, human_points, pcl_time):
         if(human_points.shape[0] == 0):
             return
         pcl = human_points
         msg = PointCloud2()
-        msg.header.stamp = self.get_clock().now().to_msg()
+        time_msg = Time()
+        time_msg.sec = int(pcl_time)
+        time_msg.nanosec = int((pcl_time - time_msg.sec) * 1e9)
+        msg.header.stamp = time_msg
         msg.header.frame_id = "world"
         msg.fields.append(PointField(name="x", offset=0, datatype=PointField.FLOAT32, count=1))
         msg.fields.append(PointField(name="y", offset=4, datatype=PointField.FLOAT32, count=1))
@@ -156,16 +165,23 @@ class HumanDetector(Node):
             idxs = idxs.reshape(-1, )
             annots[idxs] += yout[i].reshape(-1, )
             times[idxs] += 1
-        annots[times > 0] /= times[times > 0]
+        # annots[times > 0] /= times[times > 0]
         pcl = np.hstack((pcl, annots.reshape(-1, 1))).astype('float32')
         # filter pcl based on probability detection
         pcl = pcl[pcl[:, 3] > self.det_thresh]
         if(pcl.shape[0] == 0):
             return np.array([]), np.array([])
         # semantic to instanse
-        clustering = DBSCAN(eps=0.5, min_samples=30).fit(pcl[:, :3])
+        clustering = DBSCAN(eps=0.5, min_samples=15).fit(pcl[:, :3])
         cluster_labels = clustering.labels_
         human_ids = np.unique(cluster_labels[cluster_labels>=0])
+        if(self.max_hum !=None) and (human_ids.shape[0] > self.max_hum):
+            cluster_votes = Counter(cluster_labels)
+            class_ids, class_votes = np.array([key for key in cluster_votes.keys() if key >= 0]), \
+                np.array([cluster_votes[key] for key in cluster_votes.keys() if key >= 0])
+            class_ids = class_ids[np.argsort(class_votes).reshape(-1, )][::-1]
+            mask = np.in1d(cluster_labels, class_ids).reshape(cluster_labels.shape)
+            cluster_labels[~mask] = -1
         human_poses = np.zeros(shape=(human_ids.shape[0], 3))
         for i, human_id in enumerate(human_ids):
             human_poses[i, :] = pcl[cluster_labels == human_id, :3].mean(axis=0)
@@ -183,7 +199,8 @@ class HumanDetector(Node):
             if not self.curr_lidar.full(): continue
             input_lidar_info = self.curr_lidar.get()
             pcl = input_lidar_info['data']
-            pcl = pcl_voxel(pcl, voxel_size=0.12)
+            pcl_time = input_lidar_info['time']
+            pcl = pcl_voxel(pcl, voxel_size=0.2)
             if(len(human_poses) == 0):
                 cluster_idxs, pytorch_tensor, center_arr = split_pcl_to_clusters(pcl, cluster_shape=2048, min_cluster_size=40, return_pcl_gpu=True)
                 self.publish_pcl_clusters(pcl, cluster_idxs)
@@ -194,20 +211,21 @@ class HumanDetector(Node):
                 human_poses = []
                 continue
             yout, _ = self.det_model(pytorch_tensor)
+            yout = torch.sigmoid(yout)
             pytorch_tensor, yout = pytorch_tensor.detach().cpu().numpy(), yout.detach().cpu().numpy()
             human_poses, human_points = self.get_human_poses(pytorch_tensor, center_arr, pcl, yout)
-            
-            self.publish_humans(human_poses)
-            self.publish_human_seg(human_points)
-        
+            self.publish_humans(human_poses, pcl_time)
+            self.publish_human_seg(human_points, pcl_time)
+
 
 def main():
     rclpy.init()        # initialize ros2
-    model = PointNetSeg(1).to('cuda:0').eval()      # initialize model
-    model.load_state_dict(torch.load('./results/E35_v00.11.pt'))
-    # model = Pointet2().to('cuda:0').eval() 
-    # model.load_state_dict(torch.load('./results/E11_v01.07.pt'))
-    human = HumanDetector(['lidar_1', 'lidar_2'], model)        # initialize Human detector node
+    # model = PointNetSeg(1).to('cuda:0').eval()      # initialize model
+    # model.load_state_dict(torch.load('./results/E36_v01.17.pt'))
+    model = Pointet2(radius=[0.42308816, 0.87086948, 1.19641605, 1.4843377]).to('cuda:0').eval() 
+    model.load_state_dict(torch.load('./results/E36_v01.17.pt'))
+    model.eval()
+    human = HumanDetector(['lidar_1'], model, max_hum=1)        # initialize Human detector node
     human.start_detection()      # start detection
     rclpy.spin(human)           # start ros2 node
     rclpy.shutdown()        # end ros2 session
