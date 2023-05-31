a4265581070a8bf1f1fe151b4b771b30f1daafe1
Dimitrios Fotiou <fotiou.dimitris3@gmail.com>
2023-05-31 18:22:31 +0300

    commenting

--- human_pose_estimation.py
+++ human_pose_estimation.py
@@ -6,19 +6,16 @@ from sensor_msgs.msg import PointCloud2
 import queue
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 from time import time
-from scipy.spatial.transform import Rotation as rot_mat
 from utils.o3d_funcs import pcl_voxel
 from sensor_msgs.msg import PointCloud2
 from models.Pointnet import PointNetSeg
 from models.Pointnet2 import Pointet2
 import torch
-from builtin_interfaces.msg import Time
 import yaml
 from scipy.spatial import KDTree
 from utils.human_detector_utils import tfmsg_to_matrix, pcl2_to_numpy
 from utils.o3d_funcs import pcl_gicp, pcl_voxel
 from time import time
-import matplotlib.pyplot as plt
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -51,7 +48,10 @@ class HumanPoseEstimator(Node):
 
     def read_lidar_frames(self, msg):
         """
-        Read the LiDAR frames in the beginning only.
+        This is a callback function for transformation subscriber
+        Reads the LiDAR frames in the beginning only once for each
+        LiDAR frame and saves it in self.lidar_frames.
+        :param msg: transform type message.
         """
         tf_mat = tfmsg_to_matrix(msg)
         if (msg.transforms[0].child_frame_id not in list(self.lidar_frames.keys())) and \
@@ -63,6 +63,10 @@ class HumanPoseEstimator(Node):
             self.sync.registerCallback(self.read_lidar)
     
     def load_model(self, files):
+        """
+        Loads the blueprint poses of intrest.
+        :param files: the file names of blueprints inside the models folder.
+        """
         poses = []
         for file in files:
             pose = np.fromfile(f'models/{file}.npy', dtype='float32').reshape(-1, 3)
@@ -72,7 +76,10 @@ class HumanPoseEstimator(Node):
 
     def read_lidar(self, *lidar_N):
         """
-        Reads all the lidar pcls and transform them to world frame.
+        This is a callback function for lidar subscriber
+        Waits for N lidar to come in oder to merge and save
+        them in single frame as numpy format in 
+        :param *lidar_N: messages of type pointcloud queue self.curr_lidar
         """
         pcl_time = 0
         pcl_arrays = [pcl2_to_numpy(lidar_N[i], self.lidar_frames[lidar_name]) \
@@ -84,31 +91,45 @@ class HumanPoseEstimator(Node):
         self.curr_lidar.put({'data': total_pcl, 'time': pcl_time / len(self.lidar_list)})        
 
     def find_closest_pcl(self, det_time):
+        """
+        This finds the most close pointcloud to the detection time
+        that human detector published.
+        :param det_time: detection time
+        """
         cur_msg = []
         while(not self.curr_lidar.empty()):
             cur_msg = self.curr_lidar.get()
-            if(cur_msg['time'] >= det_time):
-                return cur_msg
+            if(cur_msg['time'] >= det_time): return cur_msg
             elif(cur_msg['time'] < det_time):
                 if((not self.curr_lidar.empty()) and (abs(self.curr_lidar.queue[0]['time'] - det_time) > det_time - cur_msg['time'])):
                     return cur_msg
-                else:
-                    continue
+                else: continue
         return cur_msg
     
     def find_human_pose(self, human_det, humans_seg, pcl):
+        """
+        This function finds the pose of the detected human. For now 
+        it supports only one detected human. the pose is saved in variable
+        self.current_pose
+        :param human_det: detected human position
+        :param humans_seg: the segmented points of the human
+        :param pcl: the whole pcl. This is used to fill any missing points of
+        the segmented human
+        """
         for i, human_pos in enumerate(human_det.reshape(-1, 3)):
+            # fills the segmentation of human with any missing points
             sub_pcl = pcl[np.linalg.norm(pcl - human_pos.reshape(-1, 3), axis=1) < 2.5]
             sub_pcl_tree = KDTree(sub_pcl)
             human_seg_tree = KDTree(humans_seg[i])
             idxs = sub_pcl_tree.query_ball_tree(human_seg_tree, r=0.5)
             logic_vector = np.array([True if len(idx) > 0 else False for idx in idxs])
             full_human_seg = sub_pcl[logic_vector] - human_pos
+            # Search for the best matched pose
             scores = []
             tfs = []
             for j, pose in enumerate(self.poses):
                 pose = pcl_voxel(pose, 0.02)
-                pose[:, 2] += full_human_seg[:, 2].max() - pose[:, 2].max()
+                pose[:, 2] += full_human_seg[:, 2].max() - pose[:, 2].max()     # brings the two poses in same height
                 gicp_res = pcl_gicp(pose, full_human_seg, 5)
                 scores.append(gicp_res.fitness)
                 tfs.append(gicp_res.transformation[:3, :3])
@@ -116,6 +137,14 @@ class HumanPoseEstimator(Node):
             self.current_pose = self.files[np.argmax(scores)]
 
     def check_human_stand(self, human_pos, human_seg, pcl):
+        """
+        This checks if human pose is close to a standing pose and not fall one.
+        :param human_det: detected human position
+        :param humans_seg: the segmented points of the human
+        :param pcl: the whole pcl. This is used to fill any missing points of
+        the segmented human
+        """
+        # fills the segmentation of human with any missing points
         sub_pcl = pcl[np.linalg.norm(pcl - human_pos.reshape(-1, 3), axis=1) < 2.5]
         sub_pcl_tree = KDTree(sub_pcl)
         human_seg_tree = KDTree(human_seg)
@@ -123,12 +152,20 @@ class HumanPoseEstimator(Node):
         logic_vector = np.array([True if len(idx) > 0 else False for idx in idxs])
         if(logic_vector.sum() == 0): return 0
         full_human_seg = sub_pcl[logic_vector] - human_pos
+        # Search how the detected pose is look alike the standing pose
         pose = pcl_voxel(self.poses[1], 0.02)
         pose[:, 2] += full_human_seg[:, 2].max() - pose[:, 2].max()
         gicp_res = pcl_gicp(pose, full_human_seg, 2)
         return gicp_res.fitness
 
     def human_det_call(self, human_seg, human_det):
+        """
+        This is the callback that check the human body pose periodically.
+        It can find its body pose or solve the similar task of fall detection.
+        :param human_seg: the segmented points of the human
+        :param human_det: detected human position
+        the segmented human
+        """
         det_time = human_seg.header.stamp.sec + human_seg.header.stamp.nanosec * 1e-9
         human_seg = pcl2_to_numpy(human_seg)
         human_det = pcl2_to_numpy(human_det)
@@ -152,6 +189,11 @@ class HumanPoseEstimator(Node):
             self.no_match = True if self.check_human_stand(human_det[0], human_seg[0], matched_pcl) < 0.5 else False
     
     def pose_message_pub(self):
+        """
+        Publish the pose of the human. The final publish message is depending 
+        on the application (body pose estimation or fall detection). It also checks
+        when detected last time in case of fall detection.
+        """
         if self.fall_det:
             if(((self.last_pos.qsize() > 0) and (self.curr_lidar.qsize() > 0)) and \
                 (self.curr_lidar.queue[self.curr_lidar.qsize() - 1]['time'] - self.last_pos.queue[self.last_pos.qsize() - 1]['det_time'] > 1.5)):
@@ -159,11 +201,11 @@ class HumanPoseEstimator(Node):
             else:   self.missing = False
         msg_data = ''
         if(self.current_pose!=None):
-            msg_data += f'Candidate pose {self.current_pose}\n'
+            msg_data += f'Candidate pose {self.current_pose}'
             self.current_pose = None
         if(self.fall_det):
             if(self.missing or self.no_match or self.fall_p): 
-                msg_data += f'Falling points:{self.fall_p}, Missing some time:{self.missing}, No standing pose:{self.no_match}'
+                msg_data += f'\nFalling points:{self.fall_p}, Missing some time:{self.missing}, No standing pose:{self.no_match}'
                 msg_data += '\nHuman may have fallen!'
         if(msg_data == ''): return
         msg = String()
@@ -181,9 +223,9 @@ def main():
         model = PointNetSeg(1).to('cuda:0').eval()      
         model.load_state_dict(torch.load(args['weights']))
     model.eval()
-    human = HumanPoseEstimator(args['lidar_list'], model, True, False, args)
-    rclpy.spin(human)           # start ros2 node
-    rclpy.shutdown()        # end ros2 session
+    human = HumanPoseEstimator(args['lidar_list'], model, False, True, args)
+    rclpy.spin(human)          
+    rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
--- results/README.md
+++ results/README.md
@@ -1,1 +1,2 @@
-Here training results are saved
+Here training results are saved. You can also download the best weights for Pointnet and Pointnet++ from this link: 
+https://drive.google.com/file/d/1YGyyKCt1gSrtv6ZFUQ1_-gusMBG_rnNC/view?usp=sharing
